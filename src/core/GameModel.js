d7fa3783a76579f17b2c4ba730e34904b61e4a30
coinerd <zeiler@outlook.com>
2026-02-07 05:23:32 -0500

    Refactor GameScene to use view adapters

--- src/core/GameModel.js
+++ src/core/GameModel.js
@@ -4,6 +4,9 @@
  * This model must remain free of Phaser objects and scene references.
  */
 
+import { gameEvents, GAME_EVENTS } from './EventBus.js';
+import { countPellets } from '../utils/MazeLayout.js';
+
 export default class GameModel {
     constructor({
         score = 0,
@@ -36,10 +39,36 @@ export default class GameModel {
             desiredDirection: null
         };
         this.levelConfig = null;
+        this.levelData = null;
     }
 
     setLevelConfig(levelConfig) {
         this.levelConfig = levelConfig;
+    }
+
+    setLevelData({ maze, pelletGrid }) {
+        this.levelData = { maze, pelletGrid };
+        this.setPelletCounts(countPellets(pelletGrid));
+    }
+
+    getLevelData() {
+        return this.levelData;
+    }
+
+    getLevelSnapshot() {
+        if (!this.levelData) {
+            return null;
+        }
+
+        const { maze, pelletGrid } = this.levelData;
+        return {
+            maze: maze.map((row) => [...row]),
+            pelletGrid: pelletGrid.map((row) => [...row])
+        };
+    }
+
+    getStateSnapshot() {
+        return { ...this.state };
     }
 
     getLevel() {
@@ -48,6 +77,7 @@ export default class GameModel {
 
     setHighScore(highScore) {
         this.state.highScore = highScore;
+        this.emitEvent(GAME_EVENTS.HIGH_SCORE_CHANGED, { highScore });
     }
 
     setDesiredDirection(direction) {
@@ -62,21 +92,37 @@ export default class GameModel {
 
     setPaused(isPaused) {
         this.state.isPaused = isPaused;
+        this.emitEvent(GAME_EVENTS.PAUSE_TOGGLED, { isPaused });
     }
 
     togglePaused() {
         this.state.isPaused = !this.state.isPaused;
+        this.emitEvent(GAME_EVENTS.PAUSE_TOGGLED, { isPaused: this.state.isPaused });
         return this.state.isPaused;
     }
 
     setGameOver(isGameOver) {
         this.state.isGameOver = isGameOver;
+        if (isGameOver) {
+            this.emitEvent(GAME_EVENTS.GAME_OVER, {});
+        }
     }
 
     addScore(amount) {
+        const previousHighScore = this.state.highScore;
         this.state.score += amount;
         if (this.state.score > this.state.highScore) {
             this.state.highScore = this.state.score;
+        }
+        this.emitEvent(GAME_EVENTS.SCORE_CHANGED, {
+            score: this.state.score,
+            level: this.state.level,
+            highScore: this.state.highScore
+        });
+        if (this.state.highScore > previousHighScore) {
+            this.emitEvent(GAME_EVENTS.HIGH_SCORE_CHANGED, {
+                highScore: this.state.highScore
+            });
         }
     }
 
@@ -95,6 +141,10 @@ export default class GameModel {
         if (pelletsRemaining !== undefined) {
             this.state.pelletsRemaining = pelletsRemaining;
         }
+        this.emitEvent(GAME_EVENTS.PELLET_EATEN, {
+            score,
+            pelletsRemaining: this.state.pelletsRemaining
+        });
     }
 
     onPowerPelletEaten(score, pelletsRemaining) {
@@ -103,6 +153,11 @@ export default class GameModel {
         if (pelletsRemaining !== undefined) {
             this.state.pelletsRemaining = pelletsRemaining;
         }
+        this.emitEvent(GAME_EVENTS.POWER_PELLET_EATEN, {
+            score,
+            pelletsRemaining: this.state.pelletsRemaining,
+            frightenedDuration: this.getFrightenedDuration()
+        });
     }
 
     onGhostEaten(score) {
@@ -113,26 +168,67 @@ export default class GameModel {
             this.state.maxComboGhosts,
             this.state.currentComboGhosts
         );
+        this.emitEvent(GAME_EVENTS.GHOST_EATEN, { score });
     }
 
     onPacmanDeath() {
         this.state.levelDeaths += 1;
         this.state.levelComplete = false;
+        this.emitEvent(GAME_EVENTS.LIVES_LOST, {});
     }
 
     onFruitEaten(score) {
         this.addScore(score);
         this.state.fruitsCollected += 1;
+        this.emitEvent(GAME_EVENTS.FRUIT_EATEN, { score });
     }
 
     onLevelComplete() {
         this.state.level += 1;
         this.state.levelComplete = true;
+        this.emitEvent(GAME_EVENTS.LEVEL_COMPLETE, {});
     }
 
     beginDeath() {
         this.state.isDying = true;
         this.state.deathTimer = 0;
+    }
+
+    applyPelletCollision({ pelletScore, powerPelletScore, pelletsConsumed }) {
+        if (typeof pelletsConsumed === 'number' && pelletsConsumed > 0) {
+            this.state.pelletsRemaining = Math.max(
+                0,
+                this.state.pelletsRemaining - pelletsConsumed
+            );
+        }
+
+        if (pelletScore > 0) {
+            this.onPelletEaten(pelletScore, this.state.pelletsRemaining);
+        }
+
+        if (powerPelletScore > 0) {
+            this.onPowerPelletEaten(powerPelletScore, this.state.pelletsRemaining);
+        }
+
+        if (this.state.pelletsRemaining === 0 && !this.state.levelComplete) {
+            this.onLevelComplete();
+        }
+    }
+
+    applyGhostCollision(result) {
+        if (!result) {
+            return;
+        }
+
+        if (result.type === 'ghost_eaten') {
+            this.onGhostEaten(result.score);
+            return;
+        }
+
+        if (result.type === 'pacman_died') {
+            this.onPacmanDeath();
+            this.beginDeath();
+        }
     }
 
     step(deltaSeconds, _inputState = null) {
@@ -148,6 +244,7 @@ export default class GameModel {
 
             if (this.state.lives <= 0) {
                 this.state.isGameOver = true;
+                this.emitEvent(GAME_EVENTS.GAME_OVER, {});
                 return { event: 'gameOver' };
             }
 
@@ -187,5 +284,12 @@ export default class GameModel {
     decrementLives() {
         this.state.lives -= 1;
         return this.state.lives <= 0;
+    }
+
+    emitEvent(event, payload) {
+        gameEvents.emit(event, {
+            ...payload,
+            state: this.getStateSnapshot()
+        });
     }
 }
